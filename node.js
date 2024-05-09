518f8a71b014c582683a33c1d8f1506f899293b8
PrakashNewxy <160033133+prakashnewxy1@users.noreply.github.com>
2024-05-09 07:46:11 -0400

    Update node.js

--- node.js
+++ node.js
@@ -5,5 +5,5 @@ const port = 3000;
 app.use(express.static('public')); // Serve your static files from 'public' directory
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
+    console.log(`Server running at https://prakashnewxy1.github.io/moviesimdb/:${port}/`);
 });
